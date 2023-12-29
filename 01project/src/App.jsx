1b894178c55d8510aca065be952a67e2dda01dbc
Anshuman Garg <anshumangarg120@gmail.com>
2023-12-29 20:31:35 +0530

    add new project-- 01project

---
+++ 01project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+ 
+  // let counter = 15
+
+  const addValue = () => {
+    console.log("Clicked", counter)
+   counter = counter + 1
+   setCounter(counter)
+  }
+
+  return (
+    <>
+      <h1>Project: 1 </h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}>Add Value</button>
+      <br />
+      <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
