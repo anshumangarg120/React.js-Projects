34996716abb91c26ea4dc9a6bc86606c86b3ec94
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-05 22:11:28 +0530

    fix problem

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react"
 
 function App() {
-  const [color, setColor ] = useState("olive")
+  const [color, setColor ] = useState(" ")
 
   return (
-    <div className="container">
-    <div className="content">
-      <button 
-      onClick={() => setColor("red")}
-      className="btn btn1">red</button>
-      <button className="btn btn2">red</button>
-      <button className="btn btn3">red</button>
-      <button className="btn btn4">red</button>
-      <button className="btn btn5">red</button>
-      <button className="btn btn6">red</button>
-      
+    <div className="w-full h-screen duration-200 " 
+    style={{backgroundColor: color}}>
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2  ">
+      <div className="flex flex-wrap gap-3 shadow-lg bg-white 
+      justify-center px-3 py-2 rounded-3xl">
+        <button 
+        onClick={() => setColor("red")}
+        className="outline-none  px-4 py-1 rounded-full
+         text-white shadow-lg "
+        style={{backgroundColor: "red"}}>Red</button>
+        <button 
+        onClick={() => setColor("green")}
+        className="outline-none  px-4 py-1 rounded-full
+         text-white shadow-lg "
+        style={{backgroundColor: "Green"}}>Green</button>
+        <button 
+        onClick={() => setColor("blue")}
+        className="outline-none  px-4 py-1 rounded-full
+         text-white shadow-lg "
+        style={{backgroundColor: "blue"}}>Blue</button>
+      </div>
     </div>
   </div>
     
