58359d574c7ec9ef7a40d5acfd68688a608a44a1
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-06 17:19:09 +0530

    study

--- password/src/App.jsx
+++ password/src/App.jsx
@@ -7,13 +7,51 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
-  const passwordGenerator = useCallback(fn, [length,
-  numberAllowed, numberAllowed])
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%&*~."
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
+
+
+  }, [length,
+  numberAllowed, charAllowed, setPassword])
 
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-    </>
+<div className='w-full max-w-md mx-auto shadow-md
+rounded-lg px-4 my-8 text-orange-700 bg-gray-500 '>
+  <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+<input 
+       type="text"
+       value={password}
+       className='outline-none w-full py-1 px-3'
+       placeholder='password'
+       readOnly
+       />
+       <button className='outline-none bg-blue-700 text-white 
+       px-3 py-0.5 shrink-0' >Copy</button>
+  </div>
+  <div className='flex text-sm gap-x-2'>
+    <div className='flex items-center gap-x-1'>
+      <input type="range"
+      min={6}
+      max={100}
+      value={length}
+      className='cursor-pointer' />
+
+      <label>Lenght: {length} </label>
+    </div>
+  </div>
+</div>    </>
   )
 }
 
