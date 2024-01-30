c9f621ba6b97769cdcec4a619f64856b4a337f4f
Anshuman Garg <anshumangarg120@gmail.com>
2024-01-30 11:50:13 +0530

    sty=udy

---
+++ 12MegaBlog/src/APPWRITE/auth.js
@@ -0,0 +1,21 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.conf. appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({eamil, password, name})
+    
+}
+const authService = new AuthService();
+
+export default authService;
